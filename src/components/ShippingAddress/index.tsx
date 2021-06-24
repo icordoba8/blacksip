f1169d7be718de76de519ea8821bc08852f7abaf
isabelino <icordoba8@misena.edu.co>
2021-06-24 17:47:01 -0500

    Crear condenedodes
    Se crean los contenedores de lo s componentes dirección de envío y  orden

---
+++ src/components/ShippingAddress/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.sass";
+const ShippingAddress = () => {
+  return <div className="shipping-address">dirección de envío</div>;
+};
+
+export default ShippingAddress;
---
+++ src/components/SummaryOrder/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.sass";
+const SummaryOrder = () => {
+  return <div className="sumary-order">Resumen order</div>;
+};
+
+export default SummaryOrder;
